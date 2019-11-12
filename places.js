c5c55ed80db773fcea52aad15581354437ab3de2
kyzylmonteiro <32486621+kyzylmonteiro@users.noreply.github.com>
2019-11-12 14:32:22 +0530

    debugging

--- places.js
+++ places.js
@@ -1,6 +1,6 @@
 window.onload = () => {
-    alert("finished loading- IIITD ExplorAR")
-    setTimeout(function(){ document.getElementById('splash').style.display= "none";},2000)
+    alert("finished loading- IIITD ExplorAR2")
+    setTimeout(function(){ document.getElementById('splash').style.display= "none"},2000);
     let method = 'dynamic';
 
     // if you want to statically add places, de-comment following line
